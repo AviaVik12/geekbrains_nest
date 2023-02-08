a58e6ef2f1b45b66cbf4fb5fea67231c0474bc48
Viktor Demochko <air_vic@ukr.net>
2023-02-08 20:15:36 +0100

    'lesson_1_homework'

--- src/news/news.controller.ts
+++ src/news/news.controller.ts
@@ -1,19 +1,39 @@
-import { Controller, Get, Param, Post, Body, Delete } from '@nestjs/common';
-import { News, NewsService } from './news.service';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  Delete,
+  Put,
+} from '@nestjs/common';
+import { News, NewsEdit, NewsService } from './news.service';
 
 @Controller('news')
 export class NewsController {
   constructor(private readonly newsService: NewsService) {}
 
-  @Get('/:id')
+  @Get('/detail/:id')
   get(@Param('id') id: string): News {
     let idInt = parseInt(id);
     return this.newsService.find(idInt);
   }
 
+  @Get('/all')
+  getAll(): News[] {
+    const news = this.newsService.getAll();
+    return news;
+  }
+
   @Post()
   create(@Body() news: News): News {
     return this.newsService.create(news);
+  }
+
+  @Put('/:id')
+  edit(@Param('id') id: string, @Body() news: NewsEdit): News {
+    const idInt = parseInt(id);
+    return this.newsService.edit(idInt, news);
   }
 
   @Delete('/:id')
--- src/news/news.service.ts
+++ src/news/news.service.ts
@@ -8,6 +8,13 @@ export interface News {
   countView?: number;
 }
 
+export interface NewsEdit {
+  title?: string;
+  description?: string;
+  author?: string;
+  countView?: number;
+}
+
 function getRandomInt(min: number, max: number): number {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -38,12 +45,33 @@ export class NewsService {
     return this.news.find((news) => news.id === id);
   }
 
+  getAll(): News[] {
+    return this.news;
+  }
+
+  edit(id: number, news: NewsEdit): News | undefined {
+    const indexEditableNews = this.news.findIndex((news) => news.id === id);
+
+    if (indexEditableNews !== -1) {
+      this.news[indexEditableNews] = {
+        ...this.news[indexEditableNews],
+        ...news,
+      };
+
+      return this.news[indexEditableNews];
+    }
+
+    return undefined;
+  }
+
   remove(id: News['id']): boolean {
     const indexRemoveNews = this.news.findIndex((news) => news.id === id);
+
     if (indexRemoveNews !== -1) {
       this.news.splice(indexRemoveNews, 1);
       return true;
     }
+
     return false;
   }
 }
